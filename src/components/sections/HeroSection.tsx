8860dc4c63418f427e1bc21a682cb2c9f2c5daac
Mongare Manfred <mongaremanfredo@gmail.com>
2026-07-14 19:54:55 +0530

    Lower desktop homepage hero text

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -70,7 +70,7 @@ export default function HeroSection() {
       {/* Content */}
       <div className="relative z-10 h-full flex flex-col">
         {/* Main Content */}
-        <div className="flex-1 flex items-end pb-4 section-padding content-width sm:items-center sm:pb-0">
+        <div className="flex-1 flex items-end pb-4 section-padding content-width sm:pb-10 lg:pb-14">
           <div className="max-w-3xl">
             {/* Eyebrow */}
             <div
