a4c0c13f65a37bedd96e2f4b65fc05829a211e0e
Mongare Manfred <mongaremanfredo@gmail.com>
2026-07-27 15:44:17 +0530

    Tighten homepage donation spacing

--- src/components/sections/DonationSection.tsx
+++ src/components/sections/DonationSection.tsx
@@ -14,7 +14,7 @@ const donationAccent = (title: string) => {
 
 export default function DonationSection() {
   return (
-    <section className="py-section relative overflow-hidden">
+    <section className="relative overflow-hidden py-16 md:pt-12 md:pb-24 lg:pt-14 lg:pb-28">
       {/* Background */}
       <div className="absolute inset-0">
         <img
